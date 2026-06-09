fc2bfeb151510fdb36d85aa073766661a8bea9fc
abhinmodgil <aasvi2006@gmail.com>
2026-06-09 19:58:13 +0530

    Update production contact details

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,12 +1,12 @@
 window.APP_CONFIG = {
   businessName: "Albertus Pharma",
   emailGreeting: "Hello Albertus Pharma,",
-  phoneDisplay: "",
+  phoneDisplay: "7018008582, 9418285897, 9805990300",
   email: "info@albertuspharma.com",
-  address: "H.O: Ward No. 10, Ram Nagar, Hamirpur - 177001",
+  address: "M.O.: #205, 2nd Floor, M-92/1, Choudhary Baldev Singh Complex, Munirka, New Delhi - 110067 | A.O.: #20F, SOHO, SBP City of Dreams, High Grounds, Zirakpur - 140603 | R.O.: Ward No. 10, Ram Nagar, Hamirpur (HP) - 177001",
   serviceArea: "India-wide pharmaceutical supply",
   sheetCsvUrl: "",
   googleAnalyticsId: "",
   catalogueUrl: "/assets/catalogue/albertus-pharma-catalogue.pdf",
-  mapEmbedUrl: "https://www.google.com/maps?q=Delhi%20NCR%20India&output=embed"
+  mapEmbedUrl: "https://www.google.com/maps?q=Munirka%20New%20Delhi%20110067&output=embed"
 };
